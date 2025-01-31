8e05d09f85063bcab931408cd663ee1729bd22af
Marcos Bueno <marcos_vbp@outlook.com>
2025-01-30 22:19:22 -0300

    feat: add payment verification

--- pck/payments/payments.go
+++ pck/payments/payments.go
@@ -4,18 +4,39 @@ import (
 	"fmt"
 	"time"
 
-	//"github.com/eu-micaeu/Lista3-ArqSoft/models"
+	"github.com/eu-micaeu/Lista3-ArqSoft/models"
+	"github.com/eu-micaeu/Lista3-ArqSoft/pck/orders"
 )
 
 // Função que simula um pagamento
-func Payment() {
+func Payment(order models.Order) models.Order {
 
-	fmt.Println("Pagamento sendo concluido...")
+	var option string
 
-	// Timer de 3 segundos
+	// Perguntar se o usuário deseja pagar o pedido
+	fmt.Print("\nDeseja pagar o pedido? (sim/nao): ")
+	fmt.Scan(&option)
 
-	time.Sleep(3 * time.Second)
+	// Verificar a opção escolhida
+	if option == "sim" {
 
-	fmt.Println("Pagamento concluido com sucesso!")
+		fmt.Println("\nPagamento sendo concluido...")
+
+		// Timer de 3 segundos
+
+		time.Sleep(3 * time.Second)
+
+		fmt.Println("Pagamento concluido com sucesso!")
+
+		// Alterar o estado do pedido para "Pago"
+		order = orders.PayOrder(order)
+		
+		} else {
+			
+			fmt.Println("\nPagamento cancelado.")
+
+		}
+		
+		return order
 
 }
